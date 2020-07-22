33cae24b9ee574362e3a22bbd166c310362299c2
beymedhabib <beymedhabib@gmail.com>
2020-07-22 14:56:36 +0100

    register etablisement

--- Api/EtablisementApi.js
+++ Api/EtablisementApi.js
@@ -7,7 +7,6 @@ const router = express.Router()
 router.post('/register',async(req,res)=>{
     const etablisement = Etablisement(req.body);
     const uniqueetablisement = await Etablisement.findOne({email: req.body.email});
-    
 
     if (uniqueetablisement) {
         return res.status(400).send({ message: "email already in use" });
