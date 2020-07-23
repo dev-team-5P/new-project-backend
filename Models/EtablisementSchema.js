631ecd26376270ca08b503a0ffda4532ff7994d4
beymedhabib <beymedhabib@gmail.com>
2020-07-23 13:23:24 +0100

    parametrage de compte for super admin , etablisement et condida

--- Models/EtablisementSchema.js
+++ Models/EtablisementSchema.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const Etablisement = new mongoose.Schema({
     nom : {type:String ,required:true},
     adresse : {type:String ,required:true},
-    téléphone : {type:Number ,required:true},
-    fax : {type:Number ,required:true},
+    téléphone : {type:String ,required:true},
+    fax : {type:String ,required:true},
     email : {type:String ,required:true},
     password : {type:String ,required:true},
     logo: String,
