56eeac82ca04aefd864bf7c39d4c6e7e66e953e5
beymedhabib <beymedhabib@gmail.com>
2020-08-12 12:14:16 +0100

    logo condidat

--- Models/chatSchema.js
+++ Models/chatSchema.js
@@ -4,8 +4,10 @@ var mongoose = require('mongoose');
 var privateChat = new mongoose.Schema({
     candidat1:{type: mongoose.Schema.Types.ObjectId, ref: 'Condidat'},
     candidat2:{type: mongoose.Schema.Types.ObjectId, ref: 'Condidat'},
+    
     messages:[ {
         content: String,
+        logo : String,
         candidat: {type: mongoose.Schema.Types.ObjectId, ref: 'Condidat'},
         name : {type: String ,default :""},
         createdDate: {type: Date, default: Date.now()}
